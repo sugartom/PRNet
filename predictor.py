a70794e9a2971074a271c6b0cc897a724cd0e34d
Weiwu Pang <weiwupan@usc.edu>
2019-04-29 10:46:24 -0700

    add webcam_test to prnet, using opencv

--- predictor.py
+++ predictor.py
@@ -13,15 +13,15 @@ from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model import utils
 from tensorflow.python.util import compat
 
-from grpc.beta import implementations
+import grpc
 import tensorflow as tf
 
 from tensorflow_serving.apis import predict_pb2
-from tensorflow_serving.apis import prediction_service_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
 
 from tensorflow.python.framework import tensor_util
 
-tf.app.flags.DEFINE_string('server', 'localhost:9000',
+tf.app.flags.DEFINE_string('server', 'localhost:8500',
                            'PredictionService host:port')
 tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
 
@@ -29,106 +29,144 @@ tf.app.flags.DEFINE_integer('model_version', 1, 'version number of the model.')
 FLAGS = tf.app.flags.FLAGS
 # Yitao-TLS-End
 
-def resBlock(x, num_outputs, kernel_size = 4, stride=1, activation_fn=tf.nn.relu, normalizer_fn=tcl.batch_norm, scope=None):
-    assert num_outputs%2==0 #num_outputs must be divided by channel_factor(2 here)
+
+def resBlock(x, num_outputs, kernel_size=4, stride=1, activation_fn=tf.nn.relu, normalizer_fn=tcl.batch_norm, scope=None):
+    # num_outputs must be divided by channel_factor(2 here)
+    assert num_outputs % 2 == 0
     with tf.variable_scope(scope, 'resBlock'):
         shortcut = x
         if stride != 1 or x.get_shape()[3] != num_outputs:
-            shortcut = tcl.conv2d(shortcut, num_outputs, kernel_size=1, stride=stride, 
-                        activation_fn=None, normalizer_fn=None, scope='shortcut')
-        x = tcl.conv2d(x, num_outputs/2, kernel_size=1, stride=1, padding='SAME')
-        x = tcl.conv2d(x, num_outputs/2, kernel_size=kernel_size, stride=stride, padding='SAME')
-        x = tcl.conv2d(x, num_outputs, kernel_size=1, stride=1, activation_fn=None, padding='SAME', normalizer_fn=None)
+            shortcut = tcl.conv2d(shortcut, num_outputs, kernel_size=1, stride=stride,
+                                  activation_fn=None, normalizer_fn=None, scope='shortcut')
+        x = tcl.conv2d(x, num_outputs / 2, kernel_size=1,
+                       stride=1, padding='SAME')
+        x = tcl.conv2d(x, num_outputs / 2, kernel_size=kernel_size,
+                       stride=stride, padding='SAME')
+        x = tcl.conv2d(x, num_outputs, kernel_size=1, stride=1,
+                       activation_fn=None, padding='SAME', normalizer_fn=None)
 
-        x += shortcut       
+        x += shortcut
         x = normalizer_fn(x)
         x = activation_fn(x)
     return x
 
 
 class resfcn256(object):
-    def __init__(self, resolution_inp = 256, resolution_op = 256, channel = 3, name = 'resfcn256'):
+
+    def __init__(self, resolution_inp=256, resolution_op=256, channel=3, name='resfcn256'):
         self.name = name
         self.channel = channel
         self.resolution_inp = resolution_inp
         self.resolution_op = resolution_op
 
-    def __call__(self, x, is_training = True):
+    def __call__(self, x, is_training=True):
         with tf.variable_scope(self.name) as scope:
             with arg_scope([tcl.batch_norm], is_training=is_training, scale=True):
-                with arg_scope([tcl.conv2d, tcl.conv2d_transpose], activation_fn=tf.nn.relu, 
-                                     normalizer_fn=tcl.batch_norm, 
-                                     biases_initializer=None, 
-                                     padding='SAME',
-                                     weights_regularizer=tcl.l2_regularizer(0.0002)):
-                    size = 16  
+                with arg_scope([tcl.conv2d, tcl.conv2d_transpose], activation_fn=tf.nn.relu,
+                               normalizer_fn=tcl.batch_norm,
+                               biases_initializer=None,
+                               padding='SAME',
+                               weights_regularizer=tcl.l2_regularizer(0.0002)):
+                    size = 16
                     # x: s x s x 3
-                    se = tcl.conv2d(x, num_outputs=size, kernel_size=4, stride=1) # 256 x 256 x 16
-                    se = resBlock(se, num_outputs=size * 2, kernel_size=4, stride=2) # 128 x 128 x 32
-                    se = resBlock(se, num_outputs=size * 2, kernel_size=4, stride=1) # 128 x 128 x 32
-                    se = resBlock(se, num_outputs=size * 4, kernel_size=4, stride=2) # 64 x 64 x 64
-                    se = resBlock(se, num_outputs=size * 4, kernel_size=4, stride=1) # 64 x 64 x 64
-                    se = resBlock(se, num_outputs=size * 8, kernel_size=4, stride=2) # 32 x 32 x 128
-                    se = resBlock(se, num_outputs=size * 8, kernel_size=4, stride=1) # 32 x 32 x 128
-                    se = resBlock(se, num_outputs=size * 16, kernel_size=4, stride=2) # 16 x 16 x 256
-                    se = resBlock(se, num_outputs=size * 16, kernel_size=4, stride=1) # 16 x 16 x 256
-                    se = resBlock(se, num_outputs=size * 32, kernel_size=4, stride=2) # 8 x 8 x 512
-                    se = resBlock(se, num_outputs=size * 32, kernel_size=4, stride=1) # 8 x 8 x 512
+                    se = tcl.conv2d(x, num_outputs=size,
+                                    kernel_size=4, stride=1)  # 256 x 256 x 16
+                    se = resBlock(se, num_outputs=size * 2,
+                                  kernel_size=4, stride=2)  # 128 x 128 x 32
+                    se = resBlock(se, num_outputs=size * 2,
+                                  kernel_size=4, stride=1)  # 128 x 128 x 32
+                    se = resBlock(se, num_outputs=size * 4,
+                                  kernel_size=4, stride=2)  # 64 x 64 x 64
+                    se = resBlock(se, num_outputs=size * 4,
+                                  kernel_size=4, stride=1)  # 64 x 64 x 64
+                    se = resBlock(se, num_outputs=size * 8,
+                                  kernel_size=4, stride=2)  # 32 x 32 x 128
+                    se = resBlock(se, num_outputs=size * 8,
+                                  kernel_size=4, stride=1)  # 32 x 32 x 128
+                    se = resBlock(se, num_outputs=size * 16,
+                                  kernel_size=4, stride=2)  # 16 x 16 x 256
+                    se = resBlock(se, num_outputs=size * 16,
+                                  kernel_size=4, stride=1)  # 16 x 16 x 256
+                    se = resBlock(se, num_outputs=size * 32,
+                                  kernel_size=4, stride=2)  # 8 x 8 x 512
+                    se = resBlock(se, num_outputs=size * 32,
+                                  kernel_size=4, stride=1)  # 8 x 8 x 512
 
-                    pd = tcl.conv2d_transpose(se, size * 32, 4, stride=1) # 8 x 8 x 512 
-                    pd = tcl.conv2d_transpose(pd, size * 16, 4, stride=2) # 16 x 16 x 256 
-                    pd = tcl.conv2d_transpose(pd, size * 16, 4, stride=1) # 16 x 16 x 256 
-                    pd = tcl.conv2d_transpose(pd, size * 16, 4, stride=1) # 16 x 16 x 256 
-                    pd = tcl.conv2d_transpose(pd, size * 8, 4, stride=2) # 32 x 32 x 128 
-                    pd = tcl.conv2d_transpose(pd, size * 8, 4, stride=1) # 32 x 32 x 128 
-                    pd = tcl.conv2d_transpose(pd, size * 8, 4, stride=1) # 32 x 32 x 128 
-                    pd = tcl.conv2d_transpose(pd, size * 4, 4, stride=2) # 64 x 64 x 64 
-                    pd = tcl.conv2d_transpose(pd, size * 4, 4, stride=1) # 64 x 64 x 64 
-                    pd = tcl.conv2d_transpose(pd, size * 4, 4, stride=1) # 64 x 64 x 64 
-                    
-                    pd = tcl.conv2d_transpose(pd, size * 2, 4, stride=2) # 128 x 128 x 32
-                    pd = tcl.conv2d_transpose(pd, size * 2, 4, stride=1) # 128 x 128 x 32
-                    pd = tcl.conv2d_transpose(pd, size, 4, stride=2) # 256 x 256 x 16
-                    pd = tcl.conv2d_transpose(pd, size, 4, stride=1) # 256 x 256 x 16
+                    pd = tcl.conv2d_transpose(
+                        se, size * 32, 4, stride=1)  # 8 x 8 x 512
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 16, 4, stride=2)  # 16 x 16 x 256
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 16, 4, stride=1)  # 16 x 16 x 256
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 16, 4, stride=1)  # 16 x 16 x 256
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 8, 4, stride=2)  # 32 x 32 x 128
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 8, 4, stride=1)  # 32 x 32 x 128
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 8, 4, stride=1)  # 32 x 32 x 128
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 4, 4, stride=2)  # 64 x 64 x 64
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 4, 4, stride=1)  # 64 x 64 x 64
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 4, 4, stride=1)  # 64 x 64 x 64
 
-                    pd = tcl.conv2d_transpose(pd, 3, 4, stride=1) # 256 x 256 x 3
-                    pd = tcl.conv2d_transpose(pd, 3, 4, stride=1) # 256 x 256 x 3
-                    pos = tcl.conv2d_transpose(pd, 3, 4, stride=1, activation_fn = tf.nn.sigmoid)#, padding='SAME', weights_initializer=tf.random_normal_initializer(0, 0.02))
-                                
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 2, 4, stride=2)  # 128 x 128 x 32
+                    pd = tcl.conv2d_transpose(
+                        pd, size * 2, 4, stride=1)  # 128 x 128 x 32
+                    pd = tcl.conv2d_transpose(
+                        pd, size, 4, stride=2)  # 256 x 256 x 16
+                    pd = tcl.conv2d_transpose(
+                        pd, size, 4, stride=1)  # 256 x 256 x 16
+
+                    pd = tcl.conv2d_transpose(
+                        pd, 3, 4, stride=1)  # 256 x 256 x 3
+                    pd = tcl.conv2d_transpose(
+                        pd, 3, 4, stride=1)  # 256 x 256 x 3
+                    # , padding='SAME', weights_initializer=tf.random_normal_initializer(0, 0.02))
+                    pos = tcl.conv2d_transpose(
+                        pd, 3, 4, stride=1, activation_fn=tf.nn.sigmoid)
+
                     return pos
+
     @property
     def vars(self):
         return [var for var in tf.global_variables() if self.name in var.name]
 
 
 class PosPrediction():
-    def __init__(self, resolution_inp = 256, resolution_op = 256): 
+
+    def __init__(self, resolution_inp=256, resolution_op=256):
         # -- hyper settings
         self.resolution_inp = resolution_inp
         self.resolution_op = resolution_op
-        self.MaxPos = resolution_inp*1.1
+        self.MaxPos = resolution_inp * 1.1
 
         # network type
         self.network = resfcn256(self.resolution_inp, self.resolution_op)
 
         # net forward
-        self.x = tf.placeholder(tf.float32, shape=[None, self.resolution_inp, self.resolution_inp, 3])  
-        self.x_op = self.network(self.x, is_training = False)
+        self.x = tf.placeholder(
+            tf.float32, shape=[None, self.resolution_inp, self.resolution_inp, 3])
+        self.x_op = self.network(self.x, is_training=False)
         # self.sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
         # self.sess = tf.Session()
 
         host = 'localhost'
-        port = '9000'
-        channel = implementations.insecure_channel(host, int(port))
-        self.stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+        port = '8500'
+        channel = grpc.insecure_channel('0.0.0.0:8500')
+        self.stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
-    def restore(self, model_path):        
+    def restore(self, model_path):
         # Yitao: commented this restore() call out, for debugging...
         # tf.train.Saver(self.network.vars).restore(self.sess, model_path)
         return
- 
+
     def predict(self, image):
-        # pos = self.sess.run(self.x_op, 
+        # pos = self.sess.run(self.x_op,
         #             feed_dict = {self.x: image[np.newaxis, :,:,:]})
 
         # # Yitao-TLS-Begin
@@ -161,7 +199,7 @@ class PosPrediction():
         # print('Done exporting!')
         # # Yitao-TLS-End
 
-        new_image = image[np.newaxis, :,:,:]
+        new_image = image[np.newaxis, :, :, :]
         new_image = new_image.astype(np.float32)
 
         request = predict_pb2.PredictRequest()
@@ -173,16 +211,15 @@ class PosPrediction():
 
         pos = tensor_util.MakeNdarray(result.outputs['output'])
 
-        print(pos.shape)
+        # print(pos.shape)
 
         pos = np.squeeze(pos)
 
-        print(pos.shape)
+        # print(pos.shape)
 
-        return pos*self.MaxPos
+        return pos * self.MaxPos
 
     def predict_batch(self, images):
-        pos = self.sess.run(self.x_op, 
-                    feed_dict = {self.x: images})
-        return pos*self.MaxPos
-
+        pos = self.sess.run(self.x_op,
+                            feed_dict={self.x: images})
+        return pos * self.MaxPos
